6f4f5d1c1e721d9fd4f040a362e891c9aeecd29c
jeffaristi92 <jeff.aristi@gmail.com>
2014-12-14 22:56:01 -0500

    clase menu

---
+++ OpenMind/src/Menus/MenuPrincipal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Menus;
+
+import javax.swing.JMenuBar;
+
+/**
+ *
+ * @author ARISTIZABAL
+ */
+public class MenuPrincipal {
+    JMenuBar menuPrincipal;
+    
+    void ConstruirMenu(){
+        //menuPrincipal.
+    }
+}
